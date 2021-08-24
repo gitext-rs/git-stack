92e031f199dc41bbb0a8ef2742b89152a1ed82ab
Ed Page <eopage@gmail.com>
2021-08-24 06:58:34 -0500

    fix(backup): Clarify which is top of stack

--- src/bin/git-branch-backup/main.rs
+++ src/bin/git-branch-backup/main.rs
@@ -27,7 +27,7 @@ fn run() -> proc_exit::ExitResult {
     };
 
     let colored = args.color.colored().or_else(git_stack::color::colored_env);
-    let colored_stdout = colored
+    let mut colored_stdout = colored
         .or_else(git_stack::color::colored_stdout)
         .unwrap_or(true);
     let mut colored_stderr = colored
@@ -35,6 +35,7 @@ fn run() -> proc_exit::ExitResult {
         .unwrap_or(true);
     if (colored_stdout || colored_stderr) && !yansi::Paint::enable_windows_ascii() {
         colored_stderr = false;
+        colored_stdout = false;
     }
 
     git_stack::log::init_logging(args.verbose.clone(), colored_stderr);
@@ -43,7 +44,7 @@ fn run() -> proc_exit::ExitResult {
     let push_args = args.push;
     match subcommand.unwrap_or(args::Subcommand::Push(push_args)) {
         args::Subcommand::Push(sub_args) => push(sub_args),
-        args::Subcommand::List(sub_args) => list(sub_args),
+        args::Subcommand::List(sub_args) => list(sub_args, colored_stdout),
         args::Subcommand::Clear(sub_args) => clear(sub_args),
         args::Subcommand::Drop(sub_args) => drop(sub_args),
         args::Subcommand::Pop(sub_args) => pop(sub_args),
@@ -80,14 +81,25 @@ fn push(args: args::PushArgs) -> proc_exit::ExitResult {
     Ok(())
 }
 
-fn list(args: args::ListArgs) -> proc_exit::ExitResult {
+fn list(args: args::ListArgs, colored: bool) -> proc_exit::ExitResult {
+    let palette = if colored {
+        Palette::colored()
+    } else {
+        Palette::plain()
+    };
+
     let cwd = std::env::current_dir().with_code(proc_exit::Code::USAGE_ERR)?;
     let repo = git2::Repository::discover(&cwd).with_code(proc_exit::Code::USAGE_ERR)?;
     let repo = git_stack::git::GitRepo::new(repo);
     let stack = git_stack::backup::Stack::new(&args.stack, &repo);
 
     let backups: Vec<_> = stack.iter().collect();
-    for backup_path in backups {
+    for (i, backup_path) in backups.iter().enumerate() {
+        let style = if i < backups.len() - 1 {
+            palette.info
+        } else {
+            palette.good
+        };
         let backup = match git_stack::backup::Backup::load(&backup_path) {
             Ok(backup) => backup,
             Err(err) => {
@@ -97,10 +109,18 @@ fn list(args: args::ListArgs) -> proc_exit::ExitResult {
         };
         match backup.metadata.get("message") {
             Some(message) => {
-                writeln!(std::io::stdout(), "Message: {}", message)?;
+                writeln!(
+                    std::io::stdout(),
+                    "{}",
+                    style.paint(format_args!("Message: {}", message))
+                )?;
             }
             None => {
-                writeln!(std::io::stdout(), "Path: {}", backup_path.display())?;
+                writeln!(
+                    std::io::stdout(),
+                    "{}",
+                    style.paint(format_args!("Path: {}", backup_path.display()))
+                )?;
             }
         }
         for branch in backup.branches.iter() {
@@ -115,7 +135,11 @@ fn list(args: args::ListArgs) -> proc_exit::ExitResult {
                 } else {
                     branch.name.clone()
                 };
-            writeln!(std::io::stdout(), "- {}: {}", name, summary)?;
+            writeln!(
+                std::io::stdout(),
+                "{}",
+                style.paint(format_args!("- {}: {}", name, summary))
+            )?;
         }
         writeln!(std::io::stdout())?;
     }
@@ -123,6 +147,36 @@ fn list(args: args::ListArgs) -> proc_exit::ExitResult {
     Ok(())
 }
 
+#[derive(Copy, Clone, Debug)]
+struct Palette {
+    error: yansi::Style,
+    warn: yansi::Style,
+    info: yansi::Style,
+    good: yansi::Style,
+    hint: yansi::Style,
+}
+
+impl Palette {
+    pub fn colored() -> Self {
+        Self {
+            error: yansi::Style::new(yansi::Color::Red),
+            warn: yansi::Style::new(yansi::Color::Yellow),
+            info: yansi::Style::new(yansi::Color::Blue),
+            good: yansi::Style::new(yansi::Color::Green),
+            hint: yansi::Style::new(yansi::Color::Blue).dimmed(),
+        }
+    }
+
+    pub fn plain() -> Self {
+        Self {
+            error: yansi::Style::default(),
+            warn: yansi::Style::default(),
+            info: yansi::Style::default(),
+            good: yansi::Style::default(),
+            hint: yansi::Style::default(),
+        }
+    }
+}
 fn clear(args: args::ClearArgs) -> proc_exit::ExitResult {
     let cwd = std::env::current_dir().with_code(proc_exit::Code::USAGE_ERR)?;
     let repo = git2::Repository::discover(&cwd).with_code(proc_exit::Code::USAGE_ERR)?;
