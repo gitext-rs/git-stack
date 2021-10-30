4f057561e4bb98aa44c2b3194e8b8bc9ba421b71
Ed Page <eopage@gmail.com>
2021-10-30 14:54:54 -0500

    style: Make clippy happy

--- src/bin/git-stack/stack.rs
+++ src/bin/git-stack/stack.rs
@@ -697,6 +697,7 @@ fn git_prune_development(
         eyre::bail!("Could not run `git fetch`");
     }
     let stdout = String::from_utf8(output.stdout).wrap_err("Could not run `git fetch`")?;
+    #[allow(clippy::needless_collect)]
     let remote_branches: Vec<_> = stdout
         .lines()
         .filter_map(|l| l.rsplit_once('/'))
