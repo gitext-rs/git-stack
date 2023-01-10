6ca0b409f7cfa0297eedf79fa689de8f286e0ad5
Ed Page <eopage@gmail.com>
2023-01-09 20:02:21 -0600

    fix(amend): Commit staged changes

--- tests/testsuite/amend.rs
+++ tests/testsuite/amend.rs
@@ -219,6 +219,72 @@ note: to undo, run `git branch-stash pop git-stack`
 }
 
 #[test]
+fn amend_staged() {
+    let root = snapbox::path::PathFixture::mutable_temp().unwrap();
+    let root_path = root.path().unwrap();
+    let plan = git_fixture::TodoList {
+        init: true,
+        sleep: None,
+        author: None,
+        commands: vec![
+            git_fixture::Command::Tree(git_fixture::Tree {
+                files: [("a", "a")]
+                    .into_iter()
+                    .map(|(p, c)| (p.into(), c.into()))
+                    .collect::<std::collections::HashMap<_, _>>(),
+                message: Some("A".to_owned()),
+                author: None,
+            }),
+            git_fixture::Command::Branch("main".into()),
+            git_fixture::Command::Tree(git_fixture::Tree {
+                files: [("a", "a"), ("b", "b")]
+                    .into_iter()
+                    .map(|(p, c)| (p.into(), c.into()))
+                    .collect::<std::collections::HashMap<_, _>>(),
+                message: Some("B".to_owned()),
+                author: None,
+            }),
+            git_fixture::Command::Tree(git_fixture::Tree {
+                files: [("a", "a"), ("b", "b"), ("c", "c")]
+                    .into_iter()
+                    .map(|(p, c)| (p.into(), c.into()))
+                    .collect::<std::collections::HashMap<_, _>>(),
+                message: Some("C".to_owned()),
+                author: None,
+            }),
+            git_fixture::Command::Branch("target".into()),
+        ],
+    };
+    plan.run(root_path).unwrap();
+
+    std::fs::write(root_path.join("c"), "new c").unwrap();
+
+    snapbox::cmd::Command::new("git")
+        .arg("add")
+        .arg("c")
+        .current_dir(root_path)
+        .assert()
+        .success();
+    snapbox::cmd::Command::new(snapbox::cmd::cargo_bin!("git-stack"))
+        .arg("amend")
+        .current_dir(root_path)
+        .assert()
+        .success()
+        .stdout_eq(
+            "\
+",
+        )
+        .stderr_matches(
+            "\
+Amended to [..]: C
+note: to undo, run `git branch-stash pop git-stack`
+",
+        );
+
+    root.close().unwrap();
+}
+
+#[test]
 fn amend_detached() {
     let root = snapbox::path::PathFixture::mutable_temp().unwrap();
     let root_path = root.path().unwrap();
