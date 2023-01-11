bc6fa9333dad3257a3f1b7b98e44a786b4a49345
Ed Page <eopage@gmail.com>
2023-01-11 09:01:21 -0600

    test(amend): Verify noop behavior

--- tests/testsuite/amend.rs
+++ tests/testsuite/amend.rs
@@ -1,4 +1,70 @@
 #[test]
+fn amend_noop() {
+    let root = snapbox::path::PathFixture::mutable_temp().unwrap();
+    let root_path = root.path().unwrap();
+    let plan = git_fixture::TodoList {
+        init: true,
+        sleep: None,
+        author: None,
+        commands: vec![
+            git_fixture::Command::Tree(git_fixture::Tree {
+                files: [("a", "a")]
+                    .into_iter()
+                    .map(|(p, c)| (p.into(), c.into()))
+                    .collect::<std::collections::HashMap<_, _>>(),
+                message: Some("A".to_owned()),
+                author: None,
+            }),
+            git_fixture::Command::Branch("main".into()),
+            git_fixture::Command::Tree(git_fixture::Tree {
+                files: [("a", "a"), ("b", "b")]
+                    .into_iter()
+                    .map(|(p, c)| (p.into(), c.into()))
+                    .collect::<std::collections::HashMap<_, _>>(),
+                message: Some("B".to_owned()),
+                author: None,
+            }),
+            git_fixture::Command::Tree(git_fixture::Tree {
+                files: [("a", "a"), ("b", "b"), ("c", "c")]
+                    .into_iter()
+                    .map(|(p, c)| (p.into(), c.into()))
+                    .collect::<std::collections::HashMap<_, _>>(),
+                message: Some("C".to_owned()),
+                author: None,
+            }),
+            git_fixture::Command::Branch("target".into()),
+        ],
+    };
+    plan.run(root_path).unwrap();
+
+    let repo = git2::Repository::discover(root_path).unwrap();
+    let repo = git_stack::git::GitRepo::new(repo);
+
+    let old_head_id = repo.head_commit().id;
+
+    snapbox::cmd::Command::new(snapbox::cmd::cargo_bin!("git-stack"))
+        .arg("amend")
+        .current_dir(root_path)
+        .assert()
+        .success()
+        .stdout_eq(
+            "\
+",
+        )
+        .stderr_matches(
+            "\
+Amended to [..]: C
+note: to undo, run `git branch-stash pop git-stack`
+",
+        );
+
+    let new_head_id = repo.head_commit().id;
+    assert_eq!(old_head_id, new_head_id);
+
+    root.close().unwrap();
+}
+
+#[test]
 fn reword_protected_fails() {
     let root = snapbox::path::PathFixture::mutable_temp().unwrap();
     let root_path = root.path().unwrap();
